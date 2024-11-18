6ccf9ff380d40ac33eda3fb41aa49d7077221587
Arthur <art619r@gmail.com>
2024-11-18 22:54:19 +0200

    lab8

---
+++ lab8/main.py
@@ -0,0 +1,7 @@
+import module
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+
+print(module.suml(zeroes))
+print(module.prodl(ones))
---
+++ lab8/main2.py
@@ -0,0 +1,15 @@
+from extra.iota import FunI
+from extra.good.alpha import FunA
+from extra.good.beta import FunB
+from extra.good.best.sigma import FunS
+from extra.good.best.tau import FunT
+from extra.ugly.psi import FunP
+from extra.ugly.omega import FunO
+
+print(FunI())
+print(FunA())
+print(FunB())
+print(FunS())
+print(FunT())
+print(FunP())
+print(FunO())
